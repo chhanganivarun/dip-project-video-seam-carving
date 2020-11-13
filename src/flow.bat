956e0fd243c003282cafe70d940910c4d2753bb0
trizonxjay <trizonxjay@gmail.com>
2020-11-13 22:08:46 +0530

    Fetching and pre/post processing seam for video

---
+++ src/flow.bat
@@ -0,0 +1,9 @@
+@echo off
+@REM g++ -o preproc preproc.cpp
+preproc.exe < array.txt > graph.txt
+@REM echo Preproc complete
+@REM g++ -w -o graphcut main.cpp graph.h block.h graph.cpp maxflow.cpp
+graphcut.exe < graph.txt > seamout.txt
+@REM echo Graphcut complete
+python get_seamtry.py
+@REM echo Postproc complete
---
+++ src/get_seamtry.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+with open("array.txt", "r") as f:
+	li = f.readline().split(' ')[1:]
+
+X,Y,T = [int(i) for i in li]
+I = np.loadtxt('array.txt', dtype='int').reshape(X,Y,T)
+
+def decode(num):
+	t = num%T
+	y = ((num-t)//T)%Y
+	x = (num-y*T-t)//(Y*T)
+	return (x,y,t)
+
+li = []
+with open("seamout.txt", "r") as f:
+	li = f.read().split(" ")[:-1]
+
+li = [decode(int(i)) for i in li]
+new_I = I.copy()
+# new_I = np.zeros_like(I)
+
+for point in li:
+	x,y,t = point
+	new_I[x,y,t] = -1
+
+new_I = new_I.transpose(2,1,0)
+new_I = new_I.flatten()
+new_I = (new_I[new_I!=-1]).reshape(T,Y,X-1)
+new_I = new_I.transpose(2,1,0)
+
+np.savetxt('array.txt', new_I.flatten(), fmt='%.0f', header=str(X-1)+' '+str(Y)+' '+str(T))
