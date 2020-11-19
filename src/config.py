5f530b302c360c665f470a4971007274070dff57
Aakash Dantre <aka.dantre@gmail.com>
2020-11-19 20:32:44 +0530

    update config file

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ VIDEO_PATH = "../video_samples/video.mp4"
 RESOLUTION = (240,135)
 OUT_COMPARISON_VIDEO_PATH = "../video_samples/video_out.mp4"
 OUT_RGB_VIDEO_PATH = "../video_samples/video_out_rgb.mp4"
-NUM_SEAMS = 2
+NUM_SEAMS = 6
