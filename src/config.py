38a1531fba7a760c42aa8456b99bf6d691947c18
Varun Chhangani <varunchhangani@gmail.com>
2020-11-19 22:59:56 +0530

    modify config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 COMPARISON_VIDEO_PATH = "../video_samples/video.mp4"
 VIDEO_PATH = "../video_samples/video.mp4"
-RESOLUTION = (240,135)
+RESOLUTION = (240, 135)
 OUT_COMPARISON_VIDEO_PATH = "../video_samples/video_out.mp4"
 OUT_RGB_VIDEO_PATH = "../video_samples/video_out_rgb.mp4"
-NUM_SEAMS = 6
+NUM_SEAMS = 30
